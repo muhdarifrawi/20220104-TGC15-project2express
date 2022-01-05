583811ee85c5242fe885067e44cdbca21c3c906d
arif <muhdarifrawi@gmail.com>
2022-01-05 20:27:23 +0800

    successfully connected to mongoDB.

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require("express");
 const app = express();
+const MongoUtil = require("./MongoUtil.js");
 
-app.get("/", (req,res)=>res.send(("Welcome to the main page.")))
+require("dotenv").config();
 
-app.get("/hello/:name", (req,res)=>{
-    let name = req.params.name;
-    res.send("Hello, "+ name)
-})
+// app.get("/", (req,res)=>res.send(("Welcome to the main page." + process.env.MONGO_URL)))
 
-app.listen(3000,()=>console.log("Server Ready"))
+// app.get("/hello/:name", (req,res)=>{
+//     let name = req.params.name;
+//     res.send("Hello, "+ name)
+// })
+
+
+async function main(){
+    await MongoUtil.connect(process.env.MONGO_URL, 'express-project');
+
+    app.get("/", async (req, res) => {
+        let db = MongoUtil.getDB();
+        let mainDB = await db
+            .collection("main")
+            .find()
+            .toArray();
+        console.log(mainDB);
+        res.send(mainDB);
+      });
+
+    app.listen(3000,()=>console.log("Server Ready"))
+}
+
+main();
+
